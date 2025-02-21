26ac98b64ec440a7e7e7a32ea7dc30af3b2f3855
Jadeon Sheppard <5317756+jadshep@users.noreply.github.com>
2025-02-21 22:33:45 +0000

    Prettier formatting

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,22 @@
-
-
 //
 // Types
 //
 
-export type FailableReturn<TValue, TError = Error> = {
-    readonly success: true;
-    readonly failure: false;
+export type FailableReturn<TValue, TError = Error> =
+    | {
+          readonly success: true;
+          readonly failure: false;
 
-    readonly value: TValue;
-    readonly error: undefined;
-} | {
-    readonly success: false;
-    readonly failure: true;
+          readonly value: TValue;
+          readonly error: undefined;
+      }
+    | {
+          readonly success: false;
+          readonly failure: true;
 
-    readonly value: undefined;
-    readonly error: TError;
-};
-
+          readonly value: undefined;
+          readonly error: TError;
+      };
 
 //
 // Internal utils
@@ -43,7 +42,6 @@ function createFailure<TValue, TError>(error: TError): FailableReturn<TValue, TE
     });
 }
 
-
 //
 // Public utils
 //
@@ -55,11 +53,13 @@ function createFailure<TValue, TError>(error: TError): FailableReturn<TValue, TE
  * @param args The arguments to pass to the function
  * @returns A `FailableReturn` with the function's return value or thrown error
  */
-export function trySync<A extends unknown[], TValue, TError = Error>(func: (...args: A) => TValue, ...args: A): FailableReturn<TValue, TError> {
+export function trySync<A extends unknown[], TValue, TError = Error>(
+    func: (...args: A) => TValue,
+    ...args: A
+): FailableReturn<TValue, TError> {
     try {
         return createSuccess(func(...args));
-    }
-    catch (error) {
+    } catch (error) {
         return createFailure(error as TError);
     }
 }
@@ -70,9 +70,10 @@ export function trySync<A extends unknown[], TValue, TError = Error>(func: (...a
  * @param promise The promise to handle
  * @returns A `FailableReturn` with the promise's resulting value or error
  */
-export function tryAsync<TValue, TError = Error>(promise: Promise<TValue>): Promise<FailableReturn<TValue, TError>> {
+export function tryAsync<TValue, TError = Error>(
+    promise: Promise<TValue>,
+): Promise<FailableReturn<TValue, TError>> {
     return promise
         .then(createSuccess as typeof createSuccess<TValue, TError>)
-        .catch(createFailure as typeof createFailure<TValue, TError>)
-    ;
+        .catch(createFailure as typeof createFailure<TValue, TError>);
 }
